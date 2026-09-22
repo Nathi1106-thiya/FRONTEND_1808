8bd0d9d5b3e1c1ba7b6f564bde8ef31c53507a29
Nathiya <nathi1102n@gmail.com>
2026-09-23 04:14:53 +0530

    upt

---
+++ Task/JS/Day2/assets/js/index.js
@@ -0,0 +1,210 @@
+// camelCase
+
+// userName
+// userEmail
+// userRole
+// userStatus
+// userProfile
+// firstName
+// lastName
+// phoneNumber
+// emailAddress
+// homeAddress
+// productName
+// productPrice
+// productCode
+// productImage
+// productCategory
+// orderNumber
+// orderDate
+// orderStatus
+// orderAmount
+// paymentMethod
+// accountName
+// accountType
+// accountStatus
+// loginStatus
+// accessLevel
+// buttonText
+// buttonColor
+// pageTitle
+// errorMessage
+// successMessage
+
+// Computer-related variable names
+
+
+// computerName
+// computerModel
+// operatingSystem
+// systemVersion
+// processorName
+// processorSpeed
+// memorySize
+// storageSize
+// storageType
+// screenSize
+// screenResolution
+// keyboardType
+// mouseType
+// networkStatus
+// internetSpeed
+// ipAddress
+// macAddress
+// serverName
+// serverStatus
+// databaseName
+// databaseStatus
+// fileName
+// fileSize
+// fileType
+// folderName
+// userName
+// userRole
+// loginStatus
+// passwordStatus
+// systemStatus
+
+
+// Variable declaration Return - console print - 5 print
+
+// let userName = "Nathiya";
+// let userEmail = "nathiya@gmail.com";
+// let productName = "Laptop";
+// let productPrice = 30000;
+// let boolean = true;
+
+// console.log(userName);
+// console.log(userEmail);
+// console.log(productName);
+// console.log(productPrice);
+// console.log(boolean);
+
+// init - 10 value - var , let , const
+
+// var userRole = "Student";
+// let userStatus = "Active";
+// const userProfile = "Developer";
+
+// var firstName = "Nathiya";
+// let lastName = "D";
+// const phoneNumber = "9876543210";
+
+// var emailAddress = "nathiya@gmail.com";
+// let homeAddress = "Chennai";
+// const productName = "Laptop";
+
+// let productPrice = 30000;
+
+
+// String,number,boolean,null,undefind
+
+// VAR
+
+var userRole = "Student";
+var userStatus = "Active";
+var userProfile = "Developer";
+var firstName = "Nathiya";
+
+var phoneNumber = 9876543210;
+var productPrice = 30000;
+
+var accountStatus = true;
+var loginStatus = false;
+
+var productImage = null;
+
+var homeAddress;
+
+
+// LET
+
+let lastName = "D";
+let emailAddress = "nathiya@gmail.com";
+let productName = "Laptop";
+let paymentMethod = "UPI";
+
+let orderNumber = 1001;
+let orderAmount = 25000;
+
+let orderStatus = true;
+let accountType = false;
+
+let serverStatus = null;
+
+let fileName;
+
+
+// CONST
+
+const productCategory = "Electronics";
+const accessLevel = "User";
+const pageTitle = "Home";
+const storageType = "SSD";
+
+const productCode = 101;
+const pageNumber = 10;
+
+const networkStatus = true;
+const systemStatus = false;
+
+const databaseStatus = null;
+
+const formData = undefined;
+
+
+// CONSOLE PRINT
+
+console.log(userRole);
+console.log(userStatus);
+console.log(userProfile);
+console.log(firstName);
+console.log(phoneNumber);
+console.log(productPrice);
+console.log(accountStatus);
+console.log(loginStatus);
+console.log(productImage);
+console.log(homeAddress);
+
+console.log(lastName);
+console.log(emailAddress);
+console.log(productName);
+console.log(paymentMethod);
+console.log(orderNumber);
+console.log(orderAmount);
+console.log(orderStatus);
+console.log(accountType);
+console.log(serverStatus);
+console.log(fileName);
+
+console.log(productCategory);
+console.log(accessLevel);
+console.log(pageTitle);
+console.log(storageType);
+console.log(productCode);
+console.log(pageNumber);
+console.log(networkStatus);
+console.log(systemStatus);
+console.log(databaseStatus);
+console.log(formData);
+
+// // var
+
+// var userName = "Nathiya";
+// userName = "Priya";        // Re-assignment 
+// var userName = "Kavi";     // Redeclaration 
+
+// // let
+
+// let userEmail = "nathiya@gmail.com";
+// userEmail = "priya@gmail.com";  // Re-assignment 
+
+// let userEmail = "nathiya@gmail.com";
+// let userEmail = "priya@gmail.com";  // Redeclaration never
+
+// // const
+
+// const productName = "Laptop";
+// const productName = "Mobile";  // Redeclaration never
+
+// productName = "Mobile";  // Re-assignment never
