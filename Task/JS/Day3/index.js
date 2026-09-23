4178511f2df7ab7e3facf5b718e8b741064b9611
Nathiya <nathi1102n@gmail.com>
2026-09-24 03:44:20 +0530

    upt

---
+++ Task/JS/Day3/index.js
@@ -0,0 +1,267 @@
+// let userNamee="Nathiya"
+// console.log(userNamee);
+
+
+// let firstName=10
+// let secondName=20
+// console.log(firstName+secondName);
+
+// // assignment operator
+
+// let userValue=50
+// userValue +=10
+// console.log(userValue);
+
+// userValue -=10
+
+// console.log(userValue);
+
+// // arithmetic operator
+
+// let userRole="java"
+// userRole=10
+// console.log(typeof userRole);
+//  let userProfile=10;
+//  console.log(userRole+userProfile);
+
+//  let numberFirst=10
+//  let numberSecond =20
+// numberSecond +=50
+// console.log(numberSecond);
+// total=numberFirst+numberSecond
+// console.log(total);
+
+
+// assignment operator using string
+
+// NAME
+
+let userName="a"
+console.log(userName);
+
+userName+="b"
+console.log(userName);
+
+userName+="c"
+console.log(userName);
+
+userName +="d"
+console.log(userName);
+
+userName+="e"
+console.log(userName);
+
+userName+="f"
+console.log(userName);
+
+userName+="g"
+console.log(userName);
+
+userName+="h"
+console.log(userName);
+
+userName+="i"
+console.log(userName);
+
+userName+="j"
+console.log(userName);
+
+userName+="k"
+console.log(userName);
+
+userName+="l"
+console.log(userName);
+
+userName+="m"
+console.log(userName);
+
+userName+="n"
+console.log(userName);
+
+userName+="o"
+console.log(userName);
+
+userName+="p"
+console.log(userName);
+
+userName+="q"
+console.log(userName);
+
+userName+="r"
+console.log(userName);
+
+
+// NUMBER
+
+
+let userNumber="1"
+userNumber+=" "
+console.log(userNumber);
+
+userNumber +="2"
+userNumber+=" "
+console.log(userNumber)
+
+userNumber +="3"
+userNumber+=" "
+console.log(userNumber)
+
+userNumber +="4"
+userNumber+=" "
+console.log(userNumber)
+
+userNumber +="5"
+userNumber+=" "
+console.log(userNumber)
+
+userNumber +="6"
+userNumber+=" "
+console.log(userNumber)
+
+userNumber +="7"
+userNumber+=" "
+console.log(userNumber)
+
+userNumber +="8 "
+console.log(userNumber)
+
+userNumber +="9 "
+console.log(userNumber)
+
+userNumber +="10 "
+console.log(userNumber)
+
+userNumber +="11 "
+console.log(userNumber)
+
+userNumber +="12 "
+console.log(userNumber)
+
+userNumber +="13 "
+console.log(userNumber)
+
+userNumber +="14 "
+console.log(userNumber)
+
+userNumber +="15 "
+console.log(userNumber)
+
+userNumber +="16 "
+console.log(userNumber)
+
+userNumber +="17 "
+console.log(userNumber)
+
+userNumber +="18 "
+console.log(userNumber)
+
+// let userValue ="1"
+// userValue +="2"
+// userValue +="3"
+// userValue+="4"
+// userValue+="5"
+// userValue+="6"
+// userValue+="7"
+// userValue+="8"
+
+// console.log(userValue);
+
+// SHOPPING BILL
+
+let shirtPrice=1200
+let quantity=3
+let deliveryCharge=100
+total=shirtPrice*quantity
+
+console.log(total);
+console.log(total+deliveryCharge);
+
+// EMPLOYEE SALARY
+
+let employeeSalary = 30000
+employeeSalary += 5000
+ console.log(employeeSalary);
+employeeSalary -=2000
+console.log(employeeSalary);
+
+// BANK ACCOUNT
+
+let startBal=20000
+let deposite=5000
+let withdrawal=3000
+
+startBal+=deposite
+
+console.log(startBal)
+
+startBal-=withdrawal
+
+console.log(startBal);
+
+// STUDENT MARKS
+
+let Tamil = 80
+English = 75
+Maths = 90
+Science = 85
+Computer = 95
+let Total=Tamil+English+Maths+Science+Computer
+let avgMark=Total/5
+
+console.log(Total);
+console.log(Total/5);
+
+// NUMBER OPERATIONS
+
+let number=20
+number +=10
+console.log(number);
+number *=2
+console.log(number);
+number -=20
+console.log(number);
+number /=4
+console.log(number);
+number %=3
+console.log(number);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
