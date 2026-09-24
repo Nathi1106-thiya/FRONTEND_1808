d12fd6f4681e928513e9d9c2d1913ad34d0ea42d
Nathiya <nathi1102n@gmail.com>
2026-09-25 02:40:01 +0530

    updated

---
+++ Task/JS/Day4/index.js
@@ -0,0 +1,74 @@
+// for(let a=100;a>=30;a--){
+//     console.log("Milk provided");
+// }
+
+// for(let a=100;a>=60;a--){
+//     console.log("Ice cream provided");
+// }
+
+// for(let a=100;a>=40;a--){
+//     console.log("Chocolate provided");
+// }
+
+// for(let a=1;a<=100;a++){
+//     console.log(a);
+// }
+
+// let mark = 75;
+
+// if (mark >= 90) {
+
+// console.log("Grade A");
+
+// } else if (mark >= 60) {
+
+// console.log("Grade B");
+
+// } else {
+
+// console.log("Grade C");
+
+// }
+
+// TASK-1
+
+let number=10;
+if(number>0){
+    console.log("Positive Number");
+}
+// TASK-2
+let age=17;
+if(age>=18){
+    console.log("Eligible to Vote");
+}
+else{
+    console.log("Not Eligible to Vote");
+}
+
+// TASK-3
+
+let mark=75;
+if(mark>=90){
+    console.log("Grade A");
+}
+else if(mark>=60){
+    console.log("Grade B");
+}
+else{
+    console.log("Grade C")
+}
+
+// TASK-4
+
+for(let a=1; a<=10; a++){
+    console.log(a);
+}
+
+// TASK-5
+
+for (let a=1; a<=20; a++){
+    if(a%2==0){
+        console.log(a);
+    }
+}
+
